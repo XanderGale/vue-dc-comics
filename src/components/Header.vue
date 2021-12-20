10dedafb60ca6130cf22e8a1381daf8161bc4fc5
XanderGale <alessandro.galeazzi@icloud.com>
2021-12-20 14:56:23 +0100

    just a push

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <div class="container">
+            
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss" scoped>
+    header{
+        &.container{
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: flex-start;
+        align-items: flex-start;
+        }
+    }
+</style>
