769c205bd10739c5cb5781ca78e1e8242b4b2514
XanderGale <alessandro.galeazzi@icloud.com>
2021-12-21 01:06:07 +0100

    header and main

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,16 @@
 <template>
     <header>
         <div class="container">
-            
+            <nav>
+
+                <img src="../assets/img/dc-logo.png" alt="DC Logo">
+
+                <ul>
+                    <li v-for="(link, index) in links" :key="index">
+                        <a :href="link.url">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </header>
 </template>
@@ -9,16 +18,96 @@
 <script>
 export default {
   name: 'Header',
+  data: function() {
+      return {
+          links: [
+              {
+                text: 'Characters',
+                url: '#',
+              },
+              {
+                text: 'Comics',
+                url: '#',
+              },
+              {
+                text: 'Movies',
+                url: '#',
+              },
+              {
+                text: 'Tv',
+                url: '#',
+              },
+              {
+                text: 'Games',
+                url: '#',
+              },
+              {
+                text: 'Collectibles',
+                url: '#',
+              },
+              {
+                text: 'Videos',
+                url: '#',
+              },
+              {
+                text: 'Fans',
+                url: '#',
+              },
+              {
+                text: 'News',
+                url: '#',
+              },
+              {
+                text: 'Shop',
+                url: '#',
+              },
+          ]
+      }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+@import '../style/variables.scss';
+
     header{
-        &.container{
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: flex-start;
-        align-items: flex-start;
+        background-color: white;
+        nav{
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: flex-start;
+            align-items: center;
+
+            color: $brand-second-color;
+            font-size: 12px;
+            text-transform: uppercase;
+            font-weight: 600;
+            height: 103px;
+            img{
+                height: 90%;
+            }
+            ul{
+                margin-left: auto;
+                list-style-type: none;
+                li{
+                    display: inline-block;
+                    margin: auto 10px;
+                    line-height: 100px;
+                    border-bottom: 3px solid transparent;
+                    cursor: pointer;
+                    &:hover{
+                        border-bottom: 3px solid $brand-main-color;
+                        color: $brand-main-color;
+                    }
+                    a{
+                        text-decoration: none;
+                        color: inherit;
+                        text-transform: inherit;
+                        line-height: inherit;
+                    }
+                }
+            }
         }
+        
     }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <main>
+        <div class="container">
+            Your content goes here.
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+}
+</script>
+
+<style scoped>
+    main{
+        background-color: #1c1c1c;
+        color: white;
+        font-size: 30px;
+        padding: 30px 0;
+    }
+</style>
