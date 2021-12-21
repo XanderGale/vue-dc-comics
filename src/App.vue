aec2aa65d6caf018f6858fd88e0f15294d248ba6
XanderGale <alessandro.galeazzi@icloud.com>
2021-12-22 00:57:03 +0100

    pre-footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Header/>
     <Main/>
     <Navmain/>
+    <Footer/>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
 import Navmain from './components/Navmain.vue';
+import Footer from './components/Footer.vue'
 
 export default {
   name: "App",
@@ -17,6 +19,7 @@ export default {
     Header,
     Main,
     Navmain,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,176 @@
+<template>
+    <footer>
+        <div class="pre-footer">
+            <div class="container">
+                <div class="col left">
+                    <div class="inside-col">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>
+                                <a href="#">Characters</a>
+                            </li>
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                            <li>
+                                <a href="#">Movies</a>
+                            </li>
+                            <li>
+                                <a href="#">TV</a>
+                            </li>
+                            <li>
+                                <a href="#">Games</a>
+                            </li>
+                            <li>
+                                <a href="#">Videos</a>
+                            </li>
+                            <li>
+                                <a href="#">News</a>
+                            </li>
+                        </ul>
+                        <h3>Shop</h3>
+                        <ul>
+                            <li>
+                                <a href="#">Shop DC</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop DC Collectibles</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="inside-col">
+                        <h3>DC</h3>
+                        <ul>
+                            <li>
+                                <a href="#">Therms of use</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy policy (new)</a>
+                            </li>
+                            <li>
+                                <a href="#">Ad Choices</a>
+                            </li>
+                            <li>
+                                <a href="#">Adversting</a>
+                            </li>
+                            <li>
+                                <a href="#">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#">Subscriptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Talent Workshops</a>
+                            </li>
+                            <li>
+                                <a href="#">CPSC Certificates</a>
+                            </li>
+                            <li>
+                                <a href="#">Ratings</a>
+                            </li>
+                            <li>
+                                <a href="#">Shop Help</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact us</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="inside-col">
+                        <h3>Sites</h3>
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                            </li>
+                            <li>
+                                <a href="#">Mad Magazine</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Kids</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Universe</a>
+                            </li>
+                            <li>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </div>
+                <div class="col right">
+                    <img id="footer-logo-bg" src="../assets/img/dc-logo-bg.png" alt="DC Footer Logo">
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+    footer{
+        background-color: $brand-second-color;
+        color: white;
+        .pre-footer{
+            background-image: url('../assets/img/footer-bg.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+
+            width: 100%;
+
+            overflow: hidden;
+            .container{
+                display: flex;
+                flex-flow: row nowrap;
+                justify-content: flex-start;
+                align-items: flex-start;
+                .col.left{
+                    height: 100%;
+                    width: 50%;
+
+                    padding: 30px 0;
+
+                    display: flex;
+                    flex-flow: row nowrap;
+                    justify-content: flex-start;
+                    align-items: flex-start;
+                    .inside-col{
+                        margin-right: 30px;
+                    }
+                    h3{
+                        text-transform: uppercase;
+                        margin: 20px 0;
+                    }
+                    ul{
+                        list-style-type: none;
+                        li{
+                            font-weight: 100;
+                            margin: 5px 0;
+                            a{
+                                text-decoration: none;
+                                color: inherit;
+                                font-weight: inherit;
+                            }
+                        }
+                    }
+                }
+                col.right{
+                    #footer-logo-bg{
+                        float: right;
+                        top: 0;
+                        right: 0;
+                        overflow: hidden;
+                        height: 110%;
+                    }
+                }
+            }
+        }
+    }
+</style>
